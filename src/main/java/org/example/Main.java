50ae7bce555d227f5a24d2d9f0c4d3ca939cb137
JulianD27 <165083282+JulianD27@users.noreply.github.com>
2025-03-01 15:23:40 -0500

    cambios en tipos de datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,32 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        double num1, num2, resultado;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        do {
+            System.out.println("\n--- Calculadora en Java ---");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicación");
+            System.out.println("4. División");
+            System.out.println("5. Potenciación");
+            System.out.println("6. Raíz cuadrada");
+            System.out.println("7. Módulo");
+            System.out.println("8. Conversión de Celsius a Fahrenheit");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese el primer número: ");
+                    num1 = scanner.nextDouble();
+                    System.out.print("Ingrese el segundo número: ");
+                    num2 = scanner.nextDouble();
+                    resultado = suma(num1, num2);
+                    System.out.println("Resultado: " + resultado);
+                    break;
+            }
         }
-    }
-}
